52c22c184c7fad93867264b495cd0c69a2dfb244
TomGardoni <tomg8837@gmail.com>
2021-03-10 10:20:44 +0100

    classe player

---
+++ Player.h
@@ -0,0 +1,24 @@
+#include <string>
+
+class player
+{
+    private:
+        std::string _pseudo;
+        int_score1;
+        int_score2;
+        int_score3;
+        int_score4;
+        int_score5;
+
+    public:
+        std::string getpseudo();
+        int _getscore1 ();
+        int _getscore2 ();
+        int _getscore3 ();
+        int _getscore4 ();
+        int _getscore5 ();
+        Player();
+        Player(std::string pseudo, int score_1, int score_2, int score_3, int score_4, int score_5)
+
+
+} 
---
+++ Untitled-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Player.cpp"
+using namespace std;
+
+int main()
+(
+Player Tom = Player("Tom",0,0,0,0,0)
+Player babou = Player("babou",0,0,0,0,0)
+Player Hugo = Player("Hugo",0,0,0,0,0)
+Player Antoine = Player("Antoine",0,0,0,0,0)
+Player Adien = Player("Adrien",0,0,0,0,0)
+
+return 0;
+)
